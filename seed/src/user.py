33275b4617a3f79db314f09e62ba3d78633899f1
Patricia Johansson <pj222uc@student.lnu.se>
2026-03-24 16:05:40 +0100

    Format

--- seed/src/user.py
+++ seed/src/user.py
@@ -1,5 +1,5 @@
 class User:
-  def __init__(self, username:str, email:str, password:str):
-    self.username = username
-    self.email = email
-    self.password = password
+    def __init__(self, username: str, email: str, password: str):
+        self.username = username
+        self.email = email
+        self.password = password
