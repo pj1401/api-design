8c11020ac4627aa452f42366cd54edef758f9888
Patricia Johansson <pj222uc@student.lnu.se>
2026-03-25 12:27:07 +0100

    Add flask secret key

--- api/main.py
+++ api/main.py
@@ -1,7 +1,14 @@
 from flask import Flask
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+JWT_SECRET_KEY = os.getenv("FLASK_SECRET_KEY")
 
 app = Flask(__name__)
 
+app.config["JWT_SECRET_KEY"] = JWT_SECRET_KEY
 
 @app.route("/")
 def hello_world():
