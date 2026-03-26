c95fbdbb56bf64a7e4c5359d2216bdb3181beb16
Patricia Johansson <pj222uc@student.lnu.se>
2026-03-26 10:03:35 +0100

    Update DatabaseConnectionManager

--- api/src/database-connect.py
+++
@@ -1,9 +0,0 @@
-import psycopg2
-from psycopg2 import pool
-
-
-class DatabaseConnection:
-    def __init__(self, db_config):
-        self.connection_pool = psycopg2.pool.SimpleConnectionPool(
-            minconn=1, maxconn=10, **db_config
-        )
