fefc420f85b763d85ae6a1aa0239187484466ccb
Patricia Johansson <pj222uc@student.lnu.se>
2026-03-27 10:19:28 +0100

    Update routes and config

--- api/src/config/config.py
+++ api/src/config/config.py
@@ -8,11 +8,13 @@ POSTGRES_DB = os.getenv("POSTGRES_DB")
 POSTGRES_USER = os.getenv("POSTGRES_USER")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+JWT_SECRET_KEY = os.getenv("FLASK_SECRET_KEY")
 
-db_config = {
+config = {
     "host": POSTGRES_HOST,
     "database": POSTGRES_DB,
     "user": POSTGRES_USER,
     "password": POSTGRES_PASSWORD,
     "port": POSTGRES_PORT,
+    "SECRET_KEY": JWT_SECRET_KEY,
 }
