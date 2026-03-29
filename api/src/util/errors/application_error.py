39399f4a6f2e12af978ae24f1c277d4364a06989
Patricia Johansson <pj222uc@student.lnu.se>
2026-03-29 15:23:36 +0200

    Add errors module

---
+++ api/src/util/errors/application_error.py
@@ -0,0 +1,7 @@
+class ApplicationError:
+    def __init__(self, err: BaseException, message = "An error occurred."):
+        self.err = err
+
+class UniqueViolationError(ApplicationError):
+    def __init__(self, err, message="Duplicate key value violates unique constraint."):
+        super().__init__(err, message)
